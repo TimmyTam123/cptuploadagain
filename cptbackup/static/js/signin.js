23f80b99e02e144cb0244d7bd640d9e3e9e3b644
TimmyTam Wang <timbananana@icloud.com>
2025-10-16 11:37:53 +1100

    finalfinal

--- cptbackup/static/js/signin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const infobut = document.getElementById("info");
-  const thingy = document.getElementById("thingymabobby");
-
-  infobut.addEventListener("click", function () {
-    thingy.style.visibility = "show"; // Or 'flex', 'grid', etc.
-  });
-});
